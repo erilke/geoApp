494598cf2f21837cfa758f580634194cb903873f
erilke <erilkemx@gmail.com>
2015-11-21 10:18:05 -0600

    ver0

---
+++ js/API/geoLocation.js
@@ -0,0 +1,13 @@
+var geo = {
+
+	getPosition: function(){
+		navigator.geolocation.getCurrentPosition(geo.success, geo.error);
+	},
+
+	success: function(pos){
+		fn.drawMap(pos.coords.latitude,pos.coords.longitude);
+	},
+	error: function(err){
+		alert(err.code);
+	}
+};
---
+++ js/actions.js
@@ -0,0 +1,30 @@
+var fn = {
+	ready: function (){
+		document.addEventListener("deviceready", fn.init, false);
+	},
+
+	init: function (){
+		geo.getPosition;
+	},
+
+	drawMap: function(lat,lon){
+				//Posición del mapa
+		var latlng = new google.maps.LatLng(lat, lon);
+		var myOptions = {
+			zoom: 8,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+		//Marcador
+		var marker = new google.maps.Marker({
+			position: latlng, 
+			map: map, 
+			title:"Mi posición"
+		});
+	}
+
+};
+
+// este es el document ready mínima
+$(fn.ready);
